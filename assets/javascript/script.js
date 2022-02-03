9b47a67de5a3a3c257a04e608b7a6f403af2063f
roconn <roconn25@gmail.com>
2022-02-03 21:05:11 +0000

    omdb fetch request working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,3 +4,19 @@ var omdbKey = "8f91bbc4"
 
 // API key for Marvel
 var marvelKey = "0265e94d30e0db384ca95fcdc7644186"
+
+
+
+// the queryurl below needs user input \/
+fetch(`http://www.omdbapi.com/?t=iron+man&apikey=${omdbKey}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    }
+    )
+
+
+
+
