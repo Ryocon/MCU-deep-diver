97150a0cf329892c19141486bd29ba1429b1007b
roconn <roconn25@gmail.com>
2022-02-03 21:57:04 +0000

    Start of specific api searching

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,17 +5,26 @@ var omdbKey = "8f91bbc4"
 // API key for Marvel
 var marvelKey = "0265e94d30e0db384ca95fcdc7644186"
 
+// user input keyword
+var userInput = document.getElementById('user-input').value
+
+// drop down menu for selective api searches
+var searchParameter = document.getElementById('search-params').value
+
+// search button call
+var searchBtn = document.getElementById('search-btn')
+
 
 
 // the queryurl below needs user input \/
-fetch(`http://www.omdbapi.com/?t=iron+man&apikey=${omdbKey}`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    }
-    )
+// fetch(`http://www.omdbapi.com/?t=iron+man&apikey=${omdbKey}`)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     }
+//     )
 
 
 fetch(`http://gateway.marvel.com/v1/public/comics?apikey=${marvelKey}`)
@@ -28,3 +37,27 @@ fetch(`http://gateway.marvel.com/v1/public/comics?apikey=${marvelKey}`)
     )
 
 
+
+
+//  bespoke fetch functions for each of the search parameters
+function mcuFetcher() {
+
+    if (searchParameter.value === 'mcu') {
+        fetch(`http://www.omdbapi.com/?t=` + userInput + `&apikey=${omdbKey}`)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        }
+        )
+    }
+    
+}
+
+
+
+
+
+
+// array of Marvel search terms allowed
