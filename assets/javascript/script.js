b1b3582ac2238a77cc491280f3a15141cd73f324
Mattrix01 <emjay.knight@gmail.com>
2022-02-09 17:14:21 +0000

    OMDB API returning multiple cards

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,125 +1,97 @@
-
 // API key for OMBD
-var omdbKey = "8f91bbc4"
+var omdbKey = "8f91bbc4";
 
 // API key for Marvel
-var marvelKey = "0265e94d30e0db384ca95fcdc7644186"
+var marvelKey = "0265e94d30e0db384ca95fcdc7644186";
 
 // user input keyword
-var userInput = document.getElementById('user-input')
+var userInput = document.getElementById("user-input");
 
 // drop down menu for selective api searches
-var searchParameter = document.getElementById('search-params')
+var searchParameter = document.getElementById("search-params");
 
 // search button call
-var searchBtn = document.getElementById('search-btn')
+var searchBtn = document.getElementById("search-btn");
 
 // card page
-var cardPage = document.getElementById('card-page')
+var cardPage = document.getElementById("card-page");
 
 // main page for removing + adding classes
-var mainPage = document.getElementById('main-page')
-
-
-
-
-
+var mainPage = document.getElementById("main-page");
 
 // search button with event listener
 // the function that called the fetch functions based on search params will need to be called here
-searchBtn.addEventListener("click", (event)=>{
-        event.preventDefault();
-      const userSearch = userInput.value
+searchBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const userSearch = userInput.value;
 
-        //   mainPage.classList.add('hide')
+  //   mainPage.classList.add('hide')
 
-      if (searchParameter.value === 'mcu') {
-        // removes main page elements NO ANIMATION !!!  
-        mainPage.remove()
-        mcuFetcher(userSearch)
-      } else if (searchParameter.value === 'comics') {
-        comics(userSearch);
-      } else if (searchParameter.value === 'events') {
+  if (searchParameter.value === "mcu") {
+    // removes main page elements NO ANIMATION !!!
+    mainPage.remove();
+    mcuFetcher(userSearch);
+  } else if (searchParameter.value === "comics") {
+    comics(userSearch);
+  } else if (searchParameter.value === "events") {
+  }
+  //   function here
+  else {
+    console.log("Please select an option!");
+    return;
+  }
 
-      }
-    //   function here
-      
-      else {
-          console.log('Please select an option!')
-          return
-      }
-
-      
-
-    //   if statment based on search parameters
-    // search param value finder
-    // if value == mcu > mcufetcher
-
-    });
-
-
-
+  //   if statment based on search parameters
+  // search param value finder
+  // if value == mcu > mcufetcher
+});
 
 // prototype fucntion for multiple film search cards
 function mcuFetcher(userSearch) {
   fetch(`http://www.omdbapi.com/?s=${userSearch}&apikey=${omdbKey}`)
-  .then(function (response) {
-    return response.json()
-  })
-  .then(function (filmFetch) {
-    console.log(filmFetch)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (filmFetch) {
+      console.log(filmFetch);
 
-    var noOfIterations = Math.min(filmFetch.Search.length, 9)
+      var noOfIterations = Math.min(filmFetch.Search.length, 9);
 
-debugger
-        for(var i = 0; i < noOfIterations; i++) {
-          // currently doesn't like the search[i] bit below 
-          var film = filmFetch.search[i]
+      console.log(noOfIterations);
+      for (var i = 0; i < noOfIterations; i++) {
+        // currently doesn't like the search[i] bit below
+        var film = filmFetch.Search[i];
 
+        let filmPoster = film.Poster;
+        let filmTitle = film.Title;
+        let filmYear = film.Year;
 
-          let filmPoster = film.Poster
-          let filmTitle = film.Title
-          let filmYear = film.Year
+        let cardRender = document.createElement("div");
+        let filmPosterEl = document.createElement("img");
+        let filmTitleEl = document.createElement("h2");
+        let filmYearEl = document.createElement("p");
 
-          let cardRender = document.createElement('div')  
-          let filmPosterEl = document.createElement('img')
-          let filmTitleEl = document.createElement('h2')
-          let filmYearEl = document.createElement('p')
+        cardRender.setAttribute(
+          "class",
+          "flex flex-col flex-initial max-w-md m-5 ml-8 bg-slate-400 p-5 rounded-lg text-lg decoration-1"
+        );
+        filmPosterEl.setAttribute("class", "p-1 mt-2");
+        filmTitleEl.setAttribute("class", "p-1 mt-2");
+        filmYearEl.setAttribute("class", "p-1 mt-2");
 
-          cardRender.setAttribute('class', 'flex flex-col flex-initial max-w-md m-5 ml-8 bg-slate-400 p-5 rounded-lg text-lg decoration-1')
-          filmPosterEl.setAttribute('class', 'p-1 mt-2')
-          filmTitleEl.setAttribute('class', 'p-1 mt-2')
-          filmYearEl.setAttribute('class', 'p-1 mt-2')
+        filmPosterEl.setAttribute("src", filmPoster);
+        filmPosterEl.setAttribute("class", "rounded-lg");
+        filmTitleEl.textContent = "Title: " + filmTitle;
+        filmYearEl.textContent = "Release Year: " + filmYear;
 
+        cardRender.append(filmTitleEl, filmPosterEl, filmYearEl);
 
-          filmPosterEl.setAttribute('src', filmPoster)
-          filmPosterEl.setAttribute('class', 'rounded-lg')
-          filmTitleEl.textContent = 'Title: ' + filmTitle
-          filmYearEl.textContent = 'Release Year: ' + filmYear
-
-          cardRender.append(filmTitleEl, filmPosterEl, filmYearEl)
-
-          document.getElementById('card-page').appendChild(cardRender)
-
-
-        }
-
-
-
-
-
-    }
-  )
+        document.getElementById("card-page").appendChild(cardRender);
+      }
+    });
 }
 
-
-
-
-
-
-// COMMENTED OUT FUNCTIONING SINGLE SEARCH BELOW 
-
-
+// COMMENTED OUT FUNCTIONING SINGLE SEARCH BELOW
 
 //  bespoke fetch functions for each of the search parameters
 // will need a function to call the functions individually
@@ -133,9 +105,9 @@ debugger
 //     })
 //     .then(function (data) {
 //       console.log(data);
-        
+
 //         // Data pulling (nested within the .then function)
- 
+
 //         let moviePoster = data.Poster
 
 //         let movieTitle = data.Title
@@ -151,7 +123,7 @@ debugger
 
 //         // card div to put all data in
 //         const cardRender = document.createElement('div')
-        
+
 //         const moviePosterEl = document.createElement('img')
 //         const movieTitleEl = document.createElement('h2')
 //         const releaseDateEl = document.createElement('p')
@@ -162,7 +134,6 @@ debugger
 //         releaseDateEl.setAttribute('class', 'p-1')
 //         moviePlotEl.setAttribute('class', 'p-1')
 
-
 //         // ammend
 
 //         moviePosterEl.setAttribute('src', moviePoster)
@@ -170,17 +141,13 @@ debugger
 
 //         movieTitleEl.textContent = 'Title: ' + movieTitle
 //         releaseDateEl.textContent = 'Release date: ' + releaseDate
-//         moviePlotEl.textContent = 'Plot: ' + moviePlot 
-
+//         moviePlotEl.textContent = 'Plot: ' + moviePlot
 
 //         // append
 
 //         cardRender.append(moviePosterEl, movieTitleEl, releaseDateEl, moviePlotEl)
 
 //         document.getElementById('card-page').appendChild(cardRender)
-
-
-        
 
 //         // make it clear the page after each search
 
@@ -192,89 +159,88 @@ debugger
 // }
 
 // THIS FINDS THE CHARACTER ID AND PARSES IT INTO THE comicFetcher function
+
 function comics(userSearch) {
-  fetch(`http://gateway.marvel.com/v1/public/characters?name=${userSearch}&apikey=${marvelKey}`)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (comicData) {
-        var noResults = comicData.data.results.length === 0;
+  fetch(
+    `http://gateway.marvel.com/v1/public/characters?name=${userSearch}&apikey=${marvelKey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (comicData) {
+      var noResults = comicData.data.results.length === 0;
 
-        if (noResults) {
-          console.log('Why does spiderman not work?')
-          return
-        }
-
-        var foundCharacter = comicData.data.results[0];
-
-        comicFetcher(foundCharacter.id)     
+      if (noResults) {
+        console.log("Why does spiderman not work?");
+        return;
       }
-  );
-    
-     
+
+      var foundCharacter = comicData.data.results[0];
+
+      comicFetcher(foundCharacter.id);
+    });
 }
-
-
 
 // THIS FETCHES THE COMICS AND APPENDS THEM TO DA PAGE
 function comicFetcher(characterID) {
-  fetch(`http://gateway.marvel.com/v1/public/characters/${characterID}/comics?apikey=${marvelKey}`)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (comicFetch) {
-      
+  fetch(
+    `http://gateway.marvel.com/v1/public/characters/${characterID}/comics?apikey=${marvelKey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (comicFetch) {
+      var noOfIterations = Math.min(comicFetch.data.results.length, 9);
 
-        var noOfIterations = Math.min(comicFetch.data.results.length, 9)
+      for (var i = 0; i < noOfIterations; i++) {
+        var character = comicFetch.data.results[i];
 
+        let comicTitle = character.title;
+        let comicCover =
+          character.thumbnail.path + "." + character.thumbnail.extension;
+        let comicDate = character.dates[0].date;
+        console.log("On sale date: " + comicDate);
+        let comicPages = character.pageCount;
 
-        for(var i = 0; i < noOfIterations; i++) {
-          var character = comicFetch.data.results[i]
+        const cardRender = document.createElement("div");
+        const comicCoverEl = document.createElement("img");
+        const comicTitleEl = document.createElement("h2");
+        const comicDateEl = document.createElement("p");
+        const comicPagesEl = document.createElement("p");
 
-          let comicTitle = character.title
-          let comicCover = character.thumbnail.path + "." +  character.thumbnail.extension
-          let comicDate = character.dates[0].date
-          console.log('On sale date: ' + comicDate)
-          let comicPages = character.pageCount
+        cardRender.setAttribute(
+          "class",
+          "flex flex-col flex-initial max-w-md m-5 ml-8 bg-slate-400 p-5 rounded-lg text-lg decoration-1"
+        );
+        comicTitleEl.setAttribute("class", "p-1 mt-2");
+        comicDateEl.setAttribute("class", "p-1");
+        comicPagesEl.setAttribute("class", "p-1");
 
-          const cardRender = document.createElement('div')
-          const comicCoverEl = document.createElement('img')
-          const comicTitleEl = document.createElement('h2')
-          const comicDateEl = document.createElement('p')
-          const comicPagesEl = document.createElement('p')
-          
+        comicCoverEl.setAttribute("src", comicCover);
+        comicCoverEl.setAttribute("class", "rounded-lg");
 
-          cardRender.setAttribute('class', 'flex flex-col flex-initial max-w-md m-5 ml-8 bg-slate-400 p-5 rounded-lg text-lg decoration-1')
-          comicTitleEl.setAttribute('class', 'p-1 mt-2')
-          comicDateEl.setAttribute('class', 'p-1')
-          comicPagesEl.setAttribute('class', 'p-1')
-  
-          comicCoverEl.setAttribute('src', comicCover)
-          comicCoverEl.setAttribute('class', 'rounded-lg')
-  
-          
-          // appender
-          cardRender.setAttribute('class', 'flex flex-col flex-initial max-w-md m-5 ml-8 bg-slate-400 p-5 rounded-lg text-lg decoration-1')        
-  
-          comicTitleEl.textContent = 'Title: ' + comicTitle
-          comicDateEl.textContent = 'On sale date: ' + comicDate
-          comicPagesEl.textContent = 'Page count: ' + comicPages
-  
-  
-          cardRender.append(comicTitleEl, comicCoverEl, comicDateEl, comicPagesEl)
-          document.getElementById('card-page').appendChild(cardRender)
-        }
+        // appender
+        cardRender.setAttribute(
+          "class",
+          "flex flex-col flex-initial max-w-md m-5 ml-8 bg-slate-400 p-5 rounded-lg text-lg decoration-1"
+        );
 
+        comicTitleEl.textContent = "Title: " + comicTitle;
+        comicDateEl.textContent = "On sale date: " + comicDate;
+        comicPagesEl.textContent = "Page count: " + comicPages;
+
+        cardRender.append(
+          comicTitleEl,
+          comicCoverEl,
+          comicDateEl,
+          comicPagesEl
+        );
+        document.getElementById("card-page").appendChild(cardRender);
       }
-    )
-  }
+    });
+}
 
-  
-    // backBtn = document.removeAttribute('hide')
-    // add event listenr to bckbtn
-
-
-
-
+// backBtn = document.removeAttribute('hide')
+// add event listenr to bckbtn
 
 // array of Marvel search terms allowed
