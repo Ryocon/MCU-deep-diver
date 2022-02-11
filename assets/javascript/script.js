3622de61de8c6e10dee19fbcc7ef5c181b357c92
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-02-11 13:01:07 +0000

    added character list

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,44 @@
+// Marvel Characters
+
+var marvelcharacters = ["3d Man”, “3-d Man”, “Ant-Man”, “Antman”, “Ant Man”, “Abomination”, “Chang Lam”, “Abraham Cornelius”, “Abraham Erskine”, “Adrian Toomes”, “Agatha Harkness”, “AIM agents”, “Alpha Dog”, “Amphibian”, “Squadron Supreme”, “Anaconda”, “Blanche Sitznski”, “Sitz Ski Blanche”, “Ancient One”, “Apocalypse”, “Arcade”, “Arcanna”, “Arcanna Jones”, “Arnim Zola”, “Angel”, “Warren Worthington III”, “Arsenal”, “Asp”, “Cleo Nefertiti”, “Assemble”, “Balder”, “Banshee”, “Sean Cassidy”, “Baron Zemo”, “Baron Strucker”, “Beetle”, “Ben Parker”, “Ben Reilly”, “Betty Brant”, “Betty Ross”, “Lucas Bishop”, “Black Bolt”, “Black Cat”, “Felica Hardy”, “Jack Terr Black”, “Black Knight”, “Dane Whitman”, “Black Mamba”, “Tandy Sealy”, “Black Panther”, “T Challa Udaku”, “Black Talon”, “Monica Chang”, “Black Widow", "Natasha Romanoff", "Blackheart", "Blacklash", "Marc Scott", "Blade", "Eric Cross Brooks", "Blob", "Fred J. Dukes", "Blur", "Bolivar Trask", "Bombshell", "Boomerang", "Fred Myers", "Bucky", "Bulldozer", "Bullseye", "Benjamin Pointdexter", "Bruce Banner", "Blastaar", "Cable", "Callisto", "Camellia", "Cannonball", "Sam Guthrie", "Captain America", "Steve Rogers", "Captain Atlas", "Captain Britain", "Brain Braddock", "Captain France", "Hugo Etherlinck", "Captain Italy", "Umberto Landi", "Captain Spain", "Carlos Fraile", "Captain UK", "Carnage", "Venom", "Carol Danvers", "Captain Marvel", "Catherine Mora", "Chameleon", "The Champion", "Chitauri", "Ultimate Marvel", "The Colonel", "Abdul Al-Rahman", "Colossus", "Contessa Valentina Allegra di Fontaine", "Crossbones", "Brock Rumlow", "Crystal", "Cyclops", "Scott Summers", "Clint Barton", "Hawkeye", "Dai Thomas", "Daniel Toliver", "Daredevil", "Mat Murdock", "Matthew Murdock", "Dario Agger", "CEO", "Dazzler", "Allison Blaire", "Ali Allison", "Deacon Frost", "Deadpool", "Wade Wilson", "Death Adder", "Deathlok", "Deathstrike", "Yuriko Oyama", "Derek Morgan", "Detonator", "Ricky Gibson", "Devil Dinosaur", "Diablo", "Doctor Octopus", "Otto Octavius", "Doctor Strange", "Stephen Strange", "Domino", "Drake Raven", "Doombot", "Doctor Spectrum", "Dr. Faustus", "Doctor Faustus", "Johann Fennhoff", "Dreadknight",  "Bram Velsing", "Dreamcatcher", "De Lone Wolf", "Asim Limbu", "Daisy Johnson", "Echo", "Maya Lopez", "Ego", "Electro", "Max Dillon", "Elektra", "Elektra Natchios", "Emma Frost", "White Queen", "Enchantress", "Enid Rich", "Ellie Camacho", "Falcon", "Sam Wilson", "Fancy Dan", "Dan Rubenstein", "Fandral", "Fenis",  
+"Filament", "Fin Casey", "Ivory Shadow", "Firepower", "Firestar", 
+"Liz Allen", "Elizabeth Allen", "Forge", "Fountain", "Franklin Foggy Nelson", "Foggy", "Franklin Nelson", "Franklin Richards", 
+"Franklin Storm", "Fred Flash Thompson", "Fury", "Nick Fury", 
+"Gah Lak Tus", "Gallowglass", "Gambit", "Remy LeBeau", "Gamora", 
+"Garrison Kane", "Geldoff", "Ghost", "Ghost Rider", "Ghostware", 
+"Giant Man", "Hank", "Scott Edward Harris Lang","Scott Lang", 
+"Gladiator", "Glenn Talbot","Green Goblin", "Norman Osborn", 
+"Gorgon", "Grass Hopper", "Grizzly","Gregory Stark", "Guardian", 
+"Guardian of the Galaxy", "Gwen Stacy", "Groot", "Gilgamesh", 
+"Hammer",  "Eisenhower Canty", "Hammerhead", "Hard-Drive", 
+"Havok", "Alexander ‘Alex’ Summers", "Hawk-Owl","Jack Danner", 
+"The Avengers", "Heather Hudson", "Heimdall", "Hela", "Hellcat",
+"Patsy Walker", "Hobgoblin", "Harry Osborn", "Hogun", "Howard the Duck", "Hulk", "The Hulk", "The incredible Hulk", "Robert Bruce Banner", "Bruce Banner", "Human Torch","John Storm", "Johnny Storm", "Hurricane", "Hyperion", "Iceman", "Robert Drake", "Bobby Drake", "Ikaris", "Impossible Man", "Inertia", "Invisible Woman", "Susan Storm", "Iron Fist", "Danny Rand", "Daniel Rand", "Iron Man", "Tony Stark", "Iron Monger", "Shane Obadiah", "Jackal", "James Braddock", "James Hudson", "James Rupert",  "Jim Rupert",  
+"Rhodes","War Machine", "James Buchanan ‘Bucky’ Barnes", "Jane Foster", "Edwin Jarvis", "Jeanne de Wolfe", "Jennifer Walters", 
+"Jessica Jones", "Jimmy Hudson", "Jimmy Woo", "John Ryker", "John Stacy", "J. Jonah Jameson", "Juggernaut", "Cain Marko", 
+"Justin Hammer", "Kaine", "Kang the Conqueror", "Karen Grant", "Jean Grey", "Karma",  "Xi'an Coy Mahn","Karnak","Ka-Zar", "Kenny ‘King Kong’ McFarlane", "Kingpin", "Wilson Fisk", "Kraven The Hunter", "Sergei Kravinoff", "Kree", "Korg", "Kate Bishop", "Lady Lark", "Layla miller", "Stan Lee","Leech","Leviathan", "Lieberman", "Lilandra Neramani", "Lizard", "Curtus Connors", 
+"Lockjaw", "Loki", "Longshot", "Arthur Centino", "Luke Cage", 
+"Liberatos", "Madame Web", "Magician", "Elliot Boggs", "Magneto", 
+"Erik Lensherr", "Marvel Girl", "Jean Grey", "Man-Ape", 
+"M'Baku", "Mandarin", "Man-Thing", "Ted Sallis", "Mantis","Mark Raxton", "Mary Parker", "Mary Jane Watson", "Master Mence", "Emil Burbank", "Mastermind", "Maximus", "May Parker", "Mephisto", 
+"Miles Warren", "Captain Winter", "Miles Morales", "Mister Fantastic", "Reed Richards", "Mr. Rose", "Ms. Marvel", "George Tarleton", "Mojo Adams", 
+"Mole Man", "Arthur Molekevic", "Morbius", "Vlad Tepes Dracula", 
+"Moira MacTaggert", "Morlocks", "Montana", "Moondragon", "Heather Douglas", "Medusa", "Moon Knight", "Moonknight", "Marc Spector", 
+"Multiple Man", "James Madrox", "Mysterio",  "Quentin Beck", 
+"Mystique",  "Raven Darkhölme", "Namor", "Nathaniel Richards", 
+"Nerd Hulk", "Nebula", "The New Sentinels","Nightmare", "Nightcrawler", "Kurt Wagner", "Nighthawk", "Kyle Richmond", 
+"Nihil", "Northstar", "Jean-Paul Beaubier", "Nova","Rick Jones", 
+"Nuke", "Odin", "Omega Red", "The One Above All", "Onslaught", "Orb Weaver", "Owl", "Ox", "Bruno Sanchez", "Paladin", "Marc Spector", "Pandora", "Margaret Watson", "Penultimate", "Ultimate Marvel", "Perun", "Pete Wisdom", "Phil Coulson", "Agent Coulson", 
+"SHIELD", "S.H.I.E.L.D.", "Phoenix Force", "Phoebe McAllister", "Piledriver", "Polaris", "Lorna Dane", "Power Man", "Power Princess", "Primal Screamer", "Princess Python", "The Principal", 
+"Larry Jones","Prowler", "Aaron Davis", "Proteus", "David Xavier", "Psylocke", "Elisabeth ‘Betty’ Braddock", "Pyro", "Pepper Potts", "The Punisher", "Frank Castle", "Peter Parker", "Spiderman","Spider man", "Spider-man", "Peter Quill", "Pietro Maximoff", "Quasar", "Quicksilver", "Pietro Lensherr", "Rage", "Ralph Bohner", "Wandavision", "Red Skull", "Red Ghost", "Red Guardian", "Red Wasp", "Petra Laskov", "RHINO", "Rhona Burchill", "Robbie Burchill", "Richard Parker", "Ringer", "Anthony Davis", "Robbie Robertson", "Rocket Raccoon", "Rogue", "Ronan the Accuser", "Sabra", "Sabretooth", "Victor Creed", "Samual Sterns", "Sandman", "Flint Marko", "Saquatch", "Sam Bullit", "Scarlet Witch", "Wanda Lensherr", "Schizoid Man", "Scorpion", "Sentinels", "Sentry", "Shadowcat", "Kitty Pryde", "Shang-Chi", "Shaman", "Shinobi Shaw", "Shape", "Sharon Carter", "Peggy Carter", "Agent Peggy", "Margaret Carter", "Sky-Eater", "Skrull", "Sebastian Shaw", "Spider-Girl", "May Parker", "Spider-Woman", "Spiderwoman", "Jessica Drew", "Shocker", "Storm", "Star-Lord", "Peter Jason Quill", "Sylvie/lady Loki", "Tandy Bowen",  "Tarantula", "’Dum-Dum’ Dugan", "Thaddeus E. ‘Thunderbolt’ Ross", "Thanos", "Thing", "Benjamin ‘Ben’ J. Grimm", "Thor", "Thunderball", "Tinkerer", "Elijah Stern", "Titanium Man", "Toad", "Tom Thumb", "Trey Langstrom", "Trito", "Tyrone Cash", "Ultron", "Unus", "Ben Urich", "Unicorn", "Valkyrie", "Val Cooper", "Vanisher", "Venom", "Eddie Brock", "Vindicator", "John Wraith", "Vision", "Vulture", "Blackie Drago", "Vengeance", "Volstagg", "Wasp", "Janet Pym", "Whiz Kid", "Matsuya Taki", "Wildpack", "Winter Soldier", "James ‘Bucky’ Buchanan Barnes", "Wolverine", "Logan", "James Howlett", "Jimmy Woo", "Woody", "James Rhodes", "Wong", "Whizzer", "Wrecker", "Wolfsbane", "Rahne Sinclair", "Warlock", "X-23", "Kinney Laura", "Xorn", "Kuan-Yi", "X-man", "X-Men", "Yellowjacket",  "Darren Cross", "Yondu", "Yelena Belova", "Zabu", "Zero", "Zorn", "Shen-Yi", "Zemo"
+ ];
+
+console.log (marvelcharacters);
+
+
+
+
+
 // API key for OMBD
 var omdbKey = "8f91bbc4";
 
